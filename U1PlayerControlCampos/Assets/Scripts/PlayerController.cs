96b82b774cbdee4042477d0d693292398b4a4c39
CoolGus123 <gustavo4022010@gmail.com>
2025-03-17 10:24:54 -0700

    ive finish with the bonus
    ive made the camera switcher and the 2 player

--- U1PlayerControlCampos/Assets/Scripts/PlayerController.cs
+++ U1PlayerControlCampos/Assets/Scripts/PlayerController.cs
@@ -8,16 +8,33 @@ public class PlayerController : MonoBehaviour
     private float turnSpeed = 75.0f;
     private float horizontalInput;
     private float forwardInput;
+
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
+
+    public string inputID;
     // Start is called before the first frame update
-   
+    void Start()
+    {
+        
+    }
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal" + inputID);
+        forwardInput = Input.GetAxis("Vertical" + inputID);
 
         // Move the vehicle forward based on vertical input
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         // Move the car based on horizontal input.
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+
+        if (Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
+
+        
     }
 }
